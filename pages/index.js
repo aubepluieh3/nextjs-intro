a4b99b2ceadb08ce9e89590cf141537b21df5e52
soo <aubepluieh3@naver.com>
2022-11-16 19:10:12 +0900

    Static Pre Rendering

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,14 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
+import { useState } from "react";
+
 export default function Home() {
+  const [counter, setCounter] = useState(0);
   return (
     <div>
-      <h1>Hello</h1>
+      <h1>Hello {counter}</h1>
+      <button onClick={() => setCounter((prev) => prev + 1)}>+</button>
     </div>
   );
 }
